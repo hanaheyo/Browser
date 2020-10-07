bb99e242db2fe19f8431b16ac4870797e57a8961
hanaheyo <violets5618@gmail.com>
2020-10-07 12:15:54 +0900

    Add comments on my original shoppping list app code

--- Shopping_list_application/main.js
+++ Shopping_list_application/main.js
@@ -1,13 +1,17 @@
 function getInputValue() {
     const list = document.querySelector(".container");
-    let item = document.createElement("div");
+    // 1. 사용자가 입력한 텍스트를 받아온다
     let inputVal = document.querySelector("input").value;
+    // 2. 새로운 아이템을 만든다.
+    let item = document.createElement("div");
     item.setAttribute("class", "item");
     item.innerHTML = `
     <span>${inputVal}</span>
     <i class="fas fa-trash-alt" onclick="removeItem(this);"></i>
     `
+    // 3. list에 새로 만든 아이템을 추가한다.
     list.appendChild(item);
+    //4. input을 초기화한다.
     document.querySelector("input").value = " ";
 };
 
