3a4272f6ff2da613f2aa0311d428c19ae40a3a66
hanaheyo <63185597+hanaheyo@users.noreply.github.com>
2020-10-08 23:02:02 +0900

    Shopping list app: Solution - JS
    scrollIntoView

--- Shopping_list_application/solution/solution.js
+++ Shopping_list_application/solution/solution.js
@@ -13,7 +13,9 @@ function onAdd() {
     const item = createItem(text);
     // 3. items 컨테이너 안에 새로 만든 아이템을 추가한다.
     items.appendChild(item);
-    // 4. input을 초기화한다.
+    // 4. 새로 추가된 아이템으로 스크롤링
+    item.scrollIntoView({block: "center"});
+    // 5. input을 초기화한다.
     input.value = "";
     input.focus();
 }
@@ -38,7 +40,7 @@ addBtn.addEventListener("click", () => {
     onAdd();
 });
 
-input.addEventListener("keypress", (event) => {
+input.addEventListener("keypress", event => {
     if (event.key === "Enter") {
         onAdd();
     }
