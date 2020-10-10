54aeb76a8c208379c41e3d526089c194c4600b2f
hanaheyo <violets5618@gmail.com>
2020-10-10 16:07:05 +0900

    Carrot Game : JS (Start Game)

--- Carrot_Game/main.js
+++ Carrot_Game/main.js
@@ -1,14 +1,57 @@
 'use strict';
 
 const CARROT_SIZE = 80;
-const field = document.querySelector(".game__field");
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
+const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+// 🔥게임의 상태 기억
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+    if (started) {
+        stopGame();
+    } else {
+        startGame();
+    }
+    started = !started;
+});
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+function stopGame() {
+
+}
+
+function showStopButton() {
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
 
 function initGame() {
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
     // 벌레와 당근을 생성한뒤 filed에 추가해줌
-    console.log(fieldRect);
-    addItem("carrot", 5, "img/carrot.png");
-    addItem("bug", 5, "img/bug.png");
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
@@ -17,10 +60,10 @@ function addItem(className, count, imgPath) {
     const x2 = fieldRect.width - CARROT_SIZE;
     const y2 = fieldRect.height - CARROT_SIZE;
     for (let i = 0; i < count; i++) {
-        const item = document.createElement("img");
-        item.setAttribute("class", className);
-        item.setAttribute("src", imgPath);
-        item.style.position = "absolute"
+        const item = document.createElement('img');
+        item.setAttribute('class', className);
+        item.setAttribute('src', imgPath);
+        item.style.position = 'absolute'
         const x = randomNumber(x1, x2);
         const y = randomNumber(y1, y2);
         item.style.left = `${x}px`;
@@ -33,4 +76,3 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-initGame();
