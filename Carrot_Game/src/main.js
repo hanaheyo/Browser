4dd825d207baeb362f6bf87cb5c4944b55b1307f
hanaheyo <violets5618@gmail.com>
2020-10-13 16:33:17 +0900

    Carrot Game : JS (Sound Class)

--- Carrot_Game/src/main.js
+++ Carrot_Game/src/main.js
@@ -1,6 +1,7 @@
 'use strict';
-import PopUp from './popup.js'
-import Field from './field.js'
+import PopUp from './popup.js';
+import Field from './field.js';
+import * as sound from './sound.js';
 
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
@@ -9,12 +10,6 @@ const GAME_DURATION_SEC = 5;
 const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
-
-const carrotSound = new Audio('sound/carrot_pull.mp3');
-const alertSound = new Audio('sound/alert.mp3');
-const bgSound = new Audio('sound/bg.mp3');
-const bugSound = new Audio('sound/bug_pull.mp3');
-const winSound = new Audio('sound/game_win.mp3');
 
 // 🔥게임의 상태 기억
 let started = false;
@@ -59,7 +54,7 @@ function startGame() {
     showStopButton();
     showTimerAndScore();
     startGameTimer();
-    playSound(bgSound);
+    sound.playBackground();
 }
 
 function stopGame() {
@@ -67,17 +62,17 @@ function stopGame() {
     stopGameTimer();
     hideGameButton();
     gameFinisherBanner.showWithText('REPLAY?');
-    playSound(alertSound)
-    stopSound(bgSound);
+    sound.playAlert();
+    sound.stopBackground();
 }
 
 function finishGame(win) {
     started = false;
     hideGameButton();
     if (win) {
-        playSound(winSound);
+        sound.playWin();
     } else {
-        playSound(bugSound);
+        sound.playbug();
     }
     stopGameTimer();
     stopSound(bgSound);
@@ -127,15 +122,6 @@ function initGame() {
     score = 0;
     gameScore.innerText = CARROT_COUNT;
     gameField.init();
-}
-
-function playSound(sound) {
-    sound.currentTime = 0;
-    sound.play();
-}
-
-function stopSound(sound) {
-    sound.pause();
 }
 
 function updateScoreBoard() {
